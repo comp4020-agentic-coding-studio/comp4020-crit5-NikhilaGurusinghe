643bf64294c5f150c37a1e9a40f88589b56f5795
Nikhila Gurusinghe <nikhilagurusinghe@gmail.com>
2026-08-27 14:43:11 +1000

    configuring next to do static export

---
+++ size-up-life/lib/base-path.ts
@@ -0,0 +1,4 @@
+// this is so static export-ed next.js project doesn't break
+// use this basePath in front of essentially every path in next.js code
+// (except for fonts i guess)
+export const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
--- size-up-life/next.config.ts
+++ size-up-life/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import { basePath } from "./lib/base-path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // see https://nextjs.org/docs/pages/guides/static-exports
+  output: "export",
+
+  // Change the output directory `out` -> `dist`
+  distDir: "dist",
+
+  // fixes links
+  trailingSlash: true,
+
+  // this is so links use the correct github pages url rather than the base
+  basePath,
+
+  // exposes basePath to client code so hardcoded asset src attributes
+  // (next/image doesn't auto-prepend basePath when images.unoptimized is set)
+  // can be prefixed manually
+  env: { NEXT_PUBLIC_BASE_PATH: basePath },
+
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
