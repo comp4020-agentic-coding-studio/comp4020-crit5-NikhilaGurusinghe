60c1968b28c0127d95d494629def186c8d564c2d
Nikhila Gurusinghe <nikhilagurusinghe@gmail.com>
2026-08-28 22:54:52 +1000

    navbar visual tweaks

--- size-up-life/app/components/navbar.tsx
+++ size-up-life/app/components/navbar.tsx
@@ -1,11 +1,11 @@
 export default function Navbar() {
   return (
     <header>
-      <nav className="flex flex-row items-baseline justify-between">
+      <nav className="flex flex-row items-center justify-between px-8 pt-9 md:px-9 md:pt-10">
         <h1 className="sr-only">Daily Mode</h1>
-        <p>0 <span>in a row</span></p>
+        <p className="text-5xl font-bold">0<span className="text-xl ml-[5.5px] md:ml-1.5">in a row</span></p>
 
-        <ul className="flex flex-row">
+        <ul className="flex flex-row gap-4">
           <li>Daily</li>
           <li>Reset</li>
         </ul>
