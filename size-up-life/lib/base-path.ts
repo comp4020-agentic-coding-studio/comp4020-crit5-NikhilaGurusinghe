47cc6beffabf1fd8d4cd5fd49cfa5773b0f7c847
Nikhila Gurusinghe <nikhilagurusinghe@gmail.com>
2026-08-30 23:43:50 +1000

    basepath fixes

--- size-up-life/lib/base-path.ts
+++ size-up-life/lib/base-path.ts
@@ -1,4 +1,5 @@
 // this is so static export-ed next.js project doesn't break
 // use this basePath in front of essentially every path in next.js code
 // (except for fonts i guess)
-export const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+export const basePath =
+  process.env.PAGES_BASE_PATH ?? process.env.NEXT_PUBLIC_BASE_PATH ?? "";
